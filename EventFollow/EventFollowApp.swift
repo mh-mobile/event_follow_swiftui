9566ed62883c779fd6e08a44062b71493d0f4fba
mh-mobile <mh.mobiler@gmail.com>
2021-08-11 18:58:38 +0900

    add: initial project

---
+++ EventFollow/EventFollowApp.swift
@@ -0,0 +1,17 @@
+//
+//  EventFollowApp.swift
+//  EventFollow
+//
+//  Created by mh-mobile on 2021/08/11.
+//
+
+import SwiftUI
+
+@main
+struct EventFollowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
